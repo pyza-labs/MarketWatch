394a3e753cc196f4502d8e67f5e6cbbec70f222f
Sourav Chandra <saurav.chandra1992@live.com>
2018-11-12 23:38:23 +0530

    Initial Commit

---
+++ MarketWatch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarketWatch
+//
+//  Created by Sourav Chandra on 12/11/18.
+//  Copyright © 2018 Sourav Chandra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MarketWatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MarketWatch
+//
+//  Created by Sourav Chandra on 12/11/18.
+//  Copyright © 2018 Sourav Chandra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
